8ce4deb2c4a579cd41586466152f4fbf2a293bfe
Garu84 <111827226+Garu84@users.noreply.github.com>
2026-05-24 14:06:49 -0500

    Fix case of include_colab_link in metadata

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMJGWMjnI9lpZNcDDgkobd6",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
